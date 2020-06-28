7e7ff0844a72c821ac5dd0cb908c42414b5c750d
hopeideal <cuptea@outlook.com>
2020-06-28 14:15:30 +0800

    Add React source code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from '../src/react/packages/react';
+import * as ReactDOM from '../src/react/packages/react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode >
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
